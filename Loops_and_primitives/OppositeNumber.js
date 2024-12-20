a9811fb1cc93330ad4cfda93f3ac8478f774c1e1
Vladislav Romashkan <romashkanvlad@gmail.com>
2024-12-20 15:24:21 +0300

    Opposite Number

---
+++ Loops_and_primitives/OppositeNumber.js
@@ -0,0 +1,8 @@
+function opposite(number) {
+    return number > 0 ? -number : Math.abs(number)
+}
+
+console.log(opposite(1))
+console.log(opposite(0))
+console.log(opposite(4.25))
+console.log(opposite(-12525220.3325))
