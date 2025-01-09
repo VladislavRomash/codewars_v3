cd505fcfc2fcc0e9a162e2a6c167462be06f29a8
Vladislav Romashkan <romashkanvlad@gmail.com>
2025-01-09 07:45:05 +0300

    Remove First and Last Character

---
+++ Loops_and_primitives/Remove_First_and_Last_Character.js
@@ -0,0 +1,9 @@
+function removeChar(str) {
+    return str.slice(1, str.length - 1)
+}
+
+
+console.log(removeChar('eloquent'))
+console.log(removeChar('country'))
+console.log(removeChar('person'))
+console.log(removeChar('place'))
